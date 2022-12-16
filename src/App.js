bb3ca79c37cf8b7c145a0dccbbfaefce848f9e2f
DiplomP <104051963+DiplomP@users.noreply.github.com>
2022-12-16 18:21:06 +0600

    librefresh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import MenuIcon from '@mui/icons-material/Menu';
+import Menu from './Components/Shared/Header/Menu/Menu';
+import { useState } from 'react';
+import { items } from './Components/store';
+import { BrowserRouter} from 'react-router-dom';
+import AppRouter from './Components/AppRouter';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
 
 function App() {
+  const [menuActive, setMenuActive] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <button onClick={() => setMenuActive(!menuActive)}>
+          <MenuIcon color="primary" sx={{ fontSize: 50 }}/>
+        </button>
+        <div className='logo'>
+          <a href='/'><MenuBookIcon color="primary" sx={{ fontSize: 50 }}/></a>
+        </div>
+      </nav>
+      <div className='content'>
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+      </div>
+      <Menu active={menuActive} setActive={setMenuActive} header='Этапы обучения' items={items} />
     </div>
   );
 }
